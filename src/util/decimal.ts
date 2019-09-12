0cfe35a294a648551c5c84b4e0c49246d3a3d18a
Simon Warta <simon@warta.it>
2019-09-12 10:58:16 +0200

    Update Decimal class

--- src/util/decimal.ts
+++ src/util/decimal.ts
@@ -1,7 +1,15 @@
 import BN from "bn.js";
 
+/**
+ * A type for arbitrary precision, non-negative decimals.
+ *
+ * Instances of this class are immutable.
+ */
 export class Decimal {
   public static fromUserInput(input: string, fractionalDigits: number): Decimal {
+    if (!Number.isInteger(fractionalDigits)) throw new Error("Fractional digits is not an integer");
+    if (fractionalDigits < 0) throw new Error("Fractional digits must not be negative");
+
     for (let index = 0; index < input.length; index++) {
       if (!input[index].match(/^[0-9.]$/)) {
         throw new Error(`Invalid character at position ${index + 1}`);
@@ -26,7 +34,7 @@ export class Decimal {
   private readonly quantity: BN;
   private readonly fractionalDigits: number;
 
-  public constructor(quantity: string, fractionalDigits: number) {
+  private constructor(quantity: string, fractionalDigits: number) {
     this.quantity = new BN(quantity);
     this.fractionalDigits = fractionalDigits;
   }
